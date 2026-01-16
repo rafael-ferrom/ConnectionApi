8c5f69ac6ddd0bab9da7b06de31f430e75962d72
rafael-ferrom <rafaelferromac@gmail.com>
2026-01-16 18:42:12 -0300

    style: correction in the layout

--- connectionapi/src/Layout/PageLayout.tsx
+++ connectionapi/src/Layout/PageLayout.tsx
@@ -3,33 +3,30 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 
-const PageLayout = () => {
 
+
+  const PageLayout = () => {
   return (
-    <Box 
-  sx={{ 
-    minHeight: '100vh', 
-    display: 'flex', 
-    flexDirection: 'column' 
-  }}
->
-  
-  <Box sx={{ 
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    overflow: 'auto',
-    position: 'relative', 
-    width: '100%',
-  }}>
-    <Navbar></Navbar>
-    <Outlet>
-      
-    </Outlet>
-    <Footer></Footer>
-  </Box>
-  
-</Box>
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Navbar />
+
+      <Box
+        sx={{
+          flex: 1,
+          width: "100%",
+        }}
+      >
+        <Outlet />
+      </Box>
+
+      <Footer />
+    </Box>
   );
 };
 
