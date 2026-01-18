cf9086ec6ab8206f2482b0ad0f9ce985e46675c7
rafael-ferrom <rafaelferromac@gmail.com>
2026-01-18 17:20:08 -0300

    feat: create the base for the homePage

--- connectionapi/src/components/Navbar/Navbar.tsx
+++ connectionapi/src/components/Navbar/Navbar.tsx
@@ -1,16 +1,24 @@
-
-import {  Typography } from "@mui/material"
-import { StyledNavbar } from "./style/Style"
-import HomeIcon from '@mui/icons-material/Home';
+import { Box, Typography } from "@mui/material";
+import { StyledNavbar } from "./style/Style";
+import HomeIcon from "@mui/icons-material/Home";
 
 const Navbar = () => {
   return (
     <StyledNavbar>
-        <Typography sx={{ padding: "1rem"}} variant="h4">Requisition App</Typography>
-        <HomeIcon sx={{ padding: "1rem"}}></HomeIcon>
-       
+      <Typography sx={{ padding: "1rem" }} variant="h4">
+        Requisition App
+      </Typography>
+      <Box sx={{display:"flex", justifyContent:"space-between", flexDirection:"row",alignItems: "center",width:"50%"}}>
+        <Typography sx={{backgroundColor:"red"}}>HOME</Typography>
+        <Typography>MORE</Typography>
+        <Typography>FETCH</Typography>
+        <Typography>AXIOS</Typography>
+        <Typography>THUNK</Typography>
+        <Typography>RTK</Typography>
+      </Box>
+      <HomeIcon sx={{ padding: "1rem" }}></HomeIcon>
     </StyledNavbar>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- connectionapi/src/pages/HomePage/Home.tsx
+++ connectionapi/src/pages/HomePage/Home.tsx
@@ -1,12 +1,143 @@
-import { Box, Typography } from "@mui/material"
+import {
+  Avatar,
+  Box,
+  Button,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 
+import img from "../../assets/img.jpg";
 
 const Home = () => {
   return (
     <Box>
-        <Typography>HOME</Typography>
-    </Box>
-  )
-}
+      <Box
+        sx={{
+          minHeight: "60vh",
+          backgroundImage: `url(${img})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          textAlign: "center",
+          height: "16rem",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <Typography variant="h2" color="white">
+          HOME
+        </Typography>
+        <Typography variant="h5" color="white">
+          Subtitle
+        </Typography>
+        <Button sx={{ color: "white", backgroundColor: "orange" }}>
+          Learn more
+        </Button>
+      </Box>
+      <Box>
+        <List
+          sx={{
+            backgroundColor: "green",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <ListItem sx={{display:"flex",flexDirection:"column" }}>
+            <ListItemAvatar>
+              <Avatar
+                src="/assets/img1"
+                alt="Imagem"
+              />
+            </ListItemAvatar>
 
-export default Home
+            <ListItemText sx={{textAlign:"center"}}
+              primary="Título do item"
+              secondary="Descrição ou texto complementar"
+            />
+          </ListItem>
+
+          <ListItem sx={{display:"flex",flexDirection:"column" }}>
+            <ListItemAvatar>
+              <Avatar
+                src="/imagem-exemplo.png"
+                alt="Imagem"
+              />
+            </ListItemAvatar>
+
+            <ListItemText sx={{textAlign:"center"}}
+              primary="Título do item"
+              secondary="Descrição ou texto complementar"
+            />
+          </ListItem>
+
+          <ListItem sx={{display:"flex",flexDirection:"column" }}>
+            <ListItemAvatar>
+              <Avatar
+                src="/imagem-exemplo.png"
+                alt="Imagem"
+              />
+            </ListItemAvatar>
+
+            <ListItemText sx={{textAlign:"center"}}
+              primary="Título do item"
+              secondary="Descrição ou texto complementar"
+            />
+          </ListItem>
+
+          <ListItem sx={{display:"flex",flexDirection:"column" }}>
+            <ListItemAvatar>
+              <Avatar
+                src="/imagem-exemplo.png"
+                alt="Imagem"
+              />
+            </ListItemAvatar>
+
+            <ListItemText sx={{textAlign:"center"}}
+              primary="Título do item"
+              secondary="Descrição ou texto complementar"
+            />
+          </ListItem>
+        </List>
+      </Box>
+      <Box>
+        <Typography sx={{marginBottom:"3rem", textAlign:"center"}} variant="h3">Vantagens e desvantagens</Typography>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>1</TableCell>
+                <TableCell>2</TableCell>
+                <TableCell>3</TableCell>
+                <TableCell>4</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>a</TableCell>
+                <TableCell>b</TableCell>
+                <TableCell>c</TableCell>
+                <TableCell>d</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Box>
+  );
+};
+
+export default Home;
