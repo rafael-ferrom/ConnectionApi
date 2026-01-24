8e7cb1ba4a14a61d8467a0ec79cb27f6d4383090
rafael-ferrom <rafaelferromac@gmail.com>
2026-01-24 16:31:51 -0300

    feat: implement the methods get, post, put and delete in component

--- connectionapi/src/pages/FetchPage/FetchPage.tsx
+++ connectionapi/src/pages/FetchPage/FetchPage.tsx
@@ -9,12 +9,103 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  TextField,
   Typography,
 } from "@mui/material";
 import { ColumnTable, RowsTable } from "../../utils/Consts";
+import { useEffect, useState } from "react";
+
+import { createUser, deleteUser, fetchData, updateUser } from "../../services/fetchUsers";
+import type { IUsersJPH } from "../../models/User";
+import type { CreateUserDTO } from "../../dtos/CreateUserDTO";
+import type { UpdateUserDTO } from "../../dtos/UpdateUserDTO";
 
 const FetchPage = () => {
+  const [data, setData] = useState<IUsersJPH[]>([]);
+  const [error, setError] = useState<string>("");
+  const [formData, setFormData] = useState<CreateUserDTO>({
+    name: "",
+    email: "",
+    username: "",
+    phone: "",
+    website: "",
+  });
+  const [userId, setUserId] = useState<number>(1);
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [message, setMessage] = useState<string>("")
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmitPut = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const payload: UpdateUserDTO = {
+      name,
+      email,
+    };
+
+    try {
+       await updateUser(userId, payload);
+
+    } catch (error) {
+      console.error("Erro ao atualizar usuário", error);
+    }
+  };
+
+  const handleSubmitPost = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await createUser(formData);
+
+      // Limpar o formulário após sucesso
+      setFormData({
+        name: "",
+        email: "",
+        username: "",
+        phone: "",
+        website: "",
+      });
+    } catch (err) {
+      setError(err instanceof Error ? err.message : "Erro ao criar usuário");
+    }
+  };
+
+  const handleDelete = async () => {
+  try {
+    await deleteUser(userId)
+    setMessage("Usuário removido")
+  } catch {
+    setMessage("Erro ao remover usuário")
+  }
+}
+
+  useEffect(() => {
+    async function loadData() {
+      try {
+        const result = await fetchData();
+        setData(result);
+      } catch (err) {
+        const error = err as Error;
+        setError(error.message || "Erro inesperado");
+      }
+    }
+
+    loadData();
+  }, []);
+
   const specifyRequisition = RowsTable.find((opt) => opt.name === "Fetch");
+
+  if (error) {
+    return <p style={{ color: "red" }}>{error}</p>;
+  }
 
   return (
     <Box
@@ -92,22 +183,194 @@ const FetchPage = () => {
           </Table>
         </TableContainer>
       </Box>
-      <Grid sx={{ width: "80%", height: "40rem", marginBottom:"2rem",textAlign:"center", backgroundColor:"gray"  }} container spacing={2}>
-        <Grid size={3} sx={{ border: "1px solid #ddd", padding: 2, display:"flex", flexDirection:"column", justifyContent:"space-between", alignItems:"center" }}>
+      <Grid
+        sx={{
+          width: "80%",
+          height: "40rem",
+          marginBottom: "2rem",
+          textAlign: "center",
+          backgroundColor: "gray",
+        }}
+        container
+        spacing={2}
+      >
+        <Grid
+          size={3}
+          sx={{
+            border: "1px solid #ddd",
+            padding: 2,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <Typography variant="h4">Divisao 3</Typography>
-          <Button sx={{backgroundColor:"blue", alignItems:"end", width:"10rem"}}>Get</Button>
+          {data?.map((dt) => (
+            <Typography>{dt.name}</Typography>
+          ))}
+          <Button
+            sx={{ backgroundColor: "blue", alignItems: "end", width: "10rem" }}
+          >
+            Get
+          </Button>
         </Grid>
-        <Grid size={3} sx={{ border: "1px solid #ddd", padding: 2, display:"flex", flexDirection:"column", justifyContent:"space-between", alignItems:"center" }}>
+        <Grid
+          size={3}
+          sx={{
+            border: "1px solid #ddd",
+            padding: 2,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <Typography variant="h4">Divisao 3</Typography>
-          <Button sx={{backgroundColor:"blue", alignItems:"end", width:"10rem"}}>Post</Button>
+          <Box component="form" onSubmit={handleSubmitPost}>
+            <Box>
+              <TextField
+                label="nome"
+                name="name"
+                variant="outlined"
+                fullWidth
+                value={formData.name}
+                onChange={handleChange}
+              ></TextField>
+            </Box>
+            <Box>
+              <TextField
+                label="E-mail"
+                name="email"
+                type="email"
+                variant="outlined"
+                fullWidth
+                value={formData.email}
+                onChange={handleChange}
+              ></TextField>
+            </Box>
+            <Box>
+              <TextField
+                label="username"
+                name="username"
+                type="username"
+                variant="outlined"
+                fullWidth
+                value={formData.username}
+                onChange={handleChange}
+              ></TextField>
+            </Box>
+            <Box>
+              <TextField
+                label="Website"
+                name="website"
+                type="website"
+                variant="outlined"
+                fullWidth
+                value={formData.website}
+                onChange={handleChange}
+              ></TextField>
+            </Box>
+            <Box>
+              <TextField
+                label="Phone"
+                name="phone"
+                type="phone"
+                variant="outlined"
+                fullWidth
+                value={formData.phone}
+                onChange={handleChange}
+              ></TextField>
+            </Box>
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{ width: "80%", height: "4rem", backgroundColor: "yellow" }}
+            >
+              AAAAAA
+            </Button>
+          </Box>
+          <Button
+            sx={{ backgroundColor: "blue", alignItems: "end", width: "10rem" }}
+          >
+            Post
+          </Button>
         </Grid>
-        <Grid size={3} sx={{ border: "1px solid #ddd", padding: 2, display:"flex", flexDirection:"column", justifyContent:"space-between", alignItems:"center" }}>
+        <Grid
+          size={3}
+          sx={{
+            border: "1px solid #ddd",
+            padding: 2,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <Typography variant="h4">Divisao 3</Typography>
-          <Button sx={{backgroundColor:"blue", alignItems:"end", width:"10rem"}}>Put</Button>
+          <Box component="form" onSubmit={handleSubmitPut}>
+            <Box>
+              <TextField
+                type="number"
+                value={userId}
+                label="ID"
+                onChange={(e) => setUserId(Number(e.target.value))}
+              ></TextField>
+            </Box>
+            <Box>
+              <TextField
+                type="text"
+                value={name}
+                label="Name"
+                onChange={(e) => setName(e.target.value)}
+              ></TextField>
+            </Box>
+            <Box>
+              <TextField
+                type="email"
+                value={email}
+                label="email"
+                onChange={(e) => setEmail(e.target.value)}
+              ></TextField>
+            </Box>
+            <Button type="submit"
+              sx={{ width: "80%", height: "4rem", backgroundColor: "yellow" }}
+            >
+              Put
+            </Button>
+          </Box>
+          <Button
+            sx={{ backgroundColor: "blue", alignItems: "end", width: "10rem" }}
+          >
+            Put
+          </Button>
         </Grid>
-        <Grid size={3} sx={{ border: "1px solid #ddd", padding: 2, display:"flex", flexDirection:"column", justifyContent:"space-between", alignItems:"center" }}>
+        <Grid
+          size={3}
+          sx={{
+            border: "1px solid #ddd",
+            padding: 2,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <Typography variant="h4">Divisao 3</Typography>
-          <Button sx={{backgroundColor:"blue", alignItems:"end", width:"10rem"}}>Delete</Button>
+          <Box>
+            <TextField type="number" value={userId} onChange={e => setUserId(Number(e.target.value))} label="ID do usuário">
+
+            </TextField>
+            <Button onClick={handleDelete} sx={{ width: "80%", height: "4rem", backgroundColor: "yellow" }}>
+              Delete
+            </Button>
+            {message && <Typography>{message}</Typography>}
+          </Box>
+          <Button
+            sx={{ backgroundColor: "blue", alignItems: "end", width: "10rem" }}
+          >
+            Delete
+          </Button>
         </Grid>
       </Grid>
     </Box>
