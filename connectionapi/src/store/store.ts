61b4778ba2806a20ffe2a6174b0689a7dce1454e
rafael-ferrom <rafaelferromac@gmail.com>
2026-03-06 17:09:34 -0300

    feat: create hooks for store and slices

--- connectionapi/src/store/store.ts
+++ connectionapi/src/store/store.ts
@@ -1,5 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
 
 export const store = configureStore({
     reducer:{}
 })
+
+type RootState = ReturnType<typeof store.getState>
+type Dispatch = typeof store.dispatch
+
+export const useAppDispatch = useDispatch.withTypes<Dispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
