4ba14fd97083e4241be2130c4c15028012a31949
rafael-ferrom <rafaelferromac@gmail.com>
2026-03-07 20:43:21 -0300

    fix: correction in the format of the thunks

--- connectionapi/src/services/thunkTodo.ts
+++ connectionapi/src/services/thunkTodo.ts
@@ -4,43 +4,50 @@ import { type toDo } from "../models/toDo";
 import axios from "axios";
 import type { CreateTodoDTO } from "../dtos/CreateTodoDTO";
 
-export const getThunkTodo = createAsyncThunk<toDo[]>( thunkURL, async(_, {rejectWithValue}) => {
-    try{
-        const result = await axios.get<toDo[]>(thunkURL) 
-        return result.data
+export const getThunkTodo = createAsyncThunk<toDo[]>(
+  "todos/getTodos",
+  async (_, { rejectWithValue }) => {
+    try {
+      const result = await axios.get<toDo[]>(thunkURL);
+      return result.data;
+    } catch {
+      return rejectWithValue("Erro ao buscar usuários");
     }
-    catch{
-        return rejectWithValue("Erro ao buscar usuários");
-    }
-})
+  }
+);
 
-export const createThunkTodo = createAsyncThunk<toDo, CreateTodoDTO>(thunkURL, async ( toDo, {rejectWithValue}) => {
-    try{
-        const result = await axios.post<toDo>(thunkURL, toDo)
-        return result.data
+export const createThunkTodo = createAsyncThunk<toDo, CreateTodoDTO>(
+  "todos/createTodo",
+  async (todo, { rejectWithValue }) => {
+    try {
+      const result = await axios.post<toDo>(thunkURL, todo);
+      return result.data;
+    } catch {
+      return rejectWithValue("Erro ao criar usuário");
     }
-    catch{
-        return rejectWithValue("Erro ao criar usuário");
-    }
-}) 
+  }
+);
 
-export const deleteThunkTodo = createAsyncThunk<number, number>(thunkURL, async (id, {rejectWithValue}) => {
-    try{
-        await axios.delete(`${thunkURL}/${id}`)
-        return id
+export const deleteThunkTodo = createAsyncThunk<number, number>(
+  "todos/deleteTodo",
+  async (id, { rejectWithValue }) => {
+    try {
+      await axios.delete(`${thunkURL}/${id}`);
+      return id;
+    } catch {
+      return rejectWithValue("Erro ao deletar usuário");
     }
-    catch{
-        return rejectWithValue("Erro ao criar usuário");
-    }
-} )
+  }
+);
 
-export const editThunkTodo = createAsyncThunk<toDo, toDo>(thunkURL, async(toDo,{rejectWithValue}) => {
-    try{
-        const result = await axios.put(`${thunkURL}/${toDo.id}`, toDo)
-        return result.data
+export const editThunkTodo = createAsyncThunk<toDo, toDo>(
+  "todos/editTodo",
+  async (todo, { rejectWithValue }) => {
+    try {
+      const result = await axios.put(`${thunkURL}/${todo.id}`, todo);
+      return result.data;
+    } catch {
+      return rejectWithValue("Erro ao editar usuário");
     }
-    catch{
-        return rejectWithValue("Erro ao criar usuário");
-    }
-
-})
+  }
+);
