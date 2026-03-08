133d4824b71cb17dc0893d204316cfc525ea10f9
rafael-ferrom <rafaelferromac@gmail.com>
2026-03-07 23:27:11 -0300

    feat: add the get function

---
+++ connectionapi/src/services/rtkComments.ts
@@ -0,0 +1,14 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { rtkURL } from "../utils/Url";
+import type { Comment } from "../models/Comment";
+
+export const commentServiceRtk = createApi({
+    reducerPath: "commentsRtk",
+    baseQuery: fetchBaseQuery({baseUrl: rtkURL}),
+    endpoints: (builder) => ({
+        getComments: builder.query<Comment[], void>({
+            query: () => 'comments'
+        }), 
+    })
+})
+
