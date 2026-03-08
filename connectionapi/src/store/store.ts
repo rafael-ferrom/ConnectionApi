a84a2224b1492eb71378ba07e51b545e34b9d107
rafael-ferrom <rafaelferromac@gmail.com>
2026-03-07 23:46:30 -0300

    feat: add the rtk query in the store

--- connectionapi/src/store/store.ts
+++ connectionapi/src/store/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch, useSelector } from "react-redux";
 import { todoSlice } from "../slices/TodoSlice";
+import { commentServiceRtk } from "../services/rtkComments";
 
 export const store = configureStore({
     reducer:{
-        todos: todoSlice.reducer
-    }
+        todos: todoSlice.reducer,
+        [commentServiceRtk.reducerPath]: commentServiceRtk.reducer
+    },
+
+    middleware:(getDefaultMiddleware) => getDefaultMiddleware().concat(commentServiceRtk.middleware)
 })
 
 type RootState = ReturnType<typeof store.getState>
